808a54b770a3b12b87f8199cf844633a3d13af17
Debobrota Haldar <99647439+iamdebobrota@users.noreply.github.com>
2022-08-30 00:20:15 +0530

    first commit

---
+++ Practice/app1/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Hello</h1>
+    <p>Hello P</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ Practice/app1/src/components/counter.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const counter = () => (
+  <div>
+    <button>Admission</button>
+    <button>sub</button>
+    <p>Admission</p>
+    <p>sub and mult</p>
+    <img src=""alt='' />
+    <h1>Admission</h1>
+    <h1>Admission and divided</h1>
+    <h1>Admission and divided</h1>
+  </div>
+)
+
+export default counter
