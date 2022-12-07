f7dd88f229c6f7761a7cd2fd154daad5f08eee7f
Debobrota Haldar <iamdebobrota@gmail.com>
2022-12-07 14:29:26 +0530

    changes login

--- c3_products_table/src/Routes/Login.jsx
+++ c3_products_table/src/Routes/Login.jsx
@@ -1,58 +1,58 @@
-import { useRef, useState, useContext } from 'react';
-import { Link, useNavigate } from 'react-router-dom'
-import { AuthContext } from '../Context/AuthContext';
+import { useRef, useState, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../Context/AuthContext";
 
 function Login() {
-
-  const { loginUser } = useContext(AuthContext)
+  const { loginUser } = useContext(AuthContext);
   // const email = useRef(null);
   // const password = useRef(null)
-  const [text, setText] = useState({})
-  const navigate = useNavigate()
-
+  const [text, setText] = useState({});
+  const navigate = useNavigate();
   // *handling form submit
   const HandleSubmit = (e) => {
-    e.preventDefault()
-
+    e.preventDefault();
     // const user = {
     //   email: email.current.value,
     //   password: password.current.value
     // }
-  
     fetch(`https://reqres.in/api/login`, {
       method: "POST",
       body: JSON.stringify(text),
       headers: {
-        'content-type': 'application/json'
-      }
-    }).then(res => res.json())
-      .then(res => {
+        "content-type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
         if (res.token) {
           loginUser(res.token);
-          navigate('/dashboard')
+          navigate("/dashboard");
         }
       })
-      .catch(err => console.log(err))
-  }
+      .catch((err) => console.log(err));
+  };
 
-  const handleChange=(e)=>{
-const inp= e.target.name;
-setText({
-  ...text,
-  [inp]: e.target.value
-})
-  }
+  const handleChange = (e) => {
+    const inp = e.target.name;
+    setText({
+      ...text,
+      [inp]: e.target.value,
+    });
+  };
   return (
     <div>
-      <form data-testid="login-form" onSubmit={HandleSubmit} >
+      <form data-testid="login-form" onSubmit={HandleSubmit}>
         <div>
           <label>
             Email
-            <input data-testid="email-input"
-            name="email"
-            //  ref={email} 
-            onChange={handleChange}
-             type="email" placeholder="email" />
+            <input
+              data-testid="email-input"
+              name="email"
+              //  ref={email}
+              onChange={handleChange}
+              type="email"
+              placeholder="email"
+            />
           </label>
         </div>
         <div>
--- c3_products_table/src/Routes/SingleProductPage.jsx
+++ c3_products_table/src/Routes/SingleProductPage.jsx
@@ -1,46 +1,40 @@
-import { useParams } from "react-router-dom"
-import React, { useEffect, useState } from 'react';
+import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import Loader from "../Components/Loader";
 
-
 function SingleProductPage() {
+  const { id } = useParams();
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(false);
 
-    const { id } = useParams();
-    const [data, setData] = useState({});
-    const [loading, setLoading] = useState(false)
-
-    useEffect(() => {
-        setLoading(true)
-        fetch(`https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-tech-products/${id}`)
-            .then(res => res.json())
-            .then(res => {
-                setData(res.data)
-                setLoading(false)
-            })
-            .catch(err => console.log(err))
-    }, [id])
-
-    return (
-        loading ? <Loader /> :
-            <div data-testid="products-container">
-                <div>
-                    <h3 data-testid="product-brand">{data?.brand}</h3>
-                </div>
-                <div >
-                    <img data-testid="product-image" src={data?.img} alt={data?.brand} />
-                </div>
-                <div data-testid="product-category">
-                    {data?.category}
-                </div>
-
-                <div data-testid="product-details">
-                    {data?.details}
-                </div>
-                <div data-testid="product-price">
-                    {data?.price}
-                </div>
-
-            </div>
+  useEffect(() => {
+    setLoading(true);
+    fetch(
+      `https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-tech-products/${id}`
     )
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res.data);
+        setLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, [id]);
+
+  return loading ? (
+    <Loader />
+  ) : (
+    <div data-testid="products-container">
+      <div>
+        <h3 data-testid="product-brand">{data?.brand}</h3>
+      </div>
+      <div>
+        <img data-testid="product-image" src={data?.img} alt={data?.brand} />
+      </div>
+      <div data-testid="product-category">{data?.category}</div>
+
+      <div data-testid="product-details">{data?.details}</div>
+      <div data-testid="product-price">{data?.price}</div>
+    </div>
+  );
 }
-export default SingleProductPage
+export default SingleProductPage;
