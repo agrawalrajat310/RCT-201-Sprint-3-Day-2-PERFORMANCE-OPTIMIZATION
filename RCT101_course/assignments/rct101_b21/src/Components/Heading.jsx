fa7648ede1ddd14ab3e00d389106ea11b40a596f
Debobrota Haldar <iamdebobrota@gmail.com>
2022-12-21 20:27:57 +0530

    create folder structure

---
+++ RCT101_course/assignments/rct101_b21/src/Components/Heading.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Heading() {
+  return (
+    <div>
+      <h1>Masai School</h1>
+    </div>
+  );
+}
+export default Heading;
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# IA
+<h1> IA Practice</h1>
+
+React courses practice and assignments
+
+<h3>RCT 101 </h3>
+<h3>RCT 201 </h3>
